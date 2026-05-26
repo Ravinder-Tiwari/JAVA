b777743e99d876358bfafb096c16816754eff469
Ravinder Tiwari <ravindertiwari373@gmail.com>
2026-05-26 22:06:32 +0530

    Pattern 2

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,14 +1,32 @@
 void main() {
     Scanner sc = new Scanner(System.in);
-
     int n = sc.nextInt();
+
     System.out.println("enter value of n: " + n);
 
-    for(int i = 1; i <= n; i++){
-       for(int j = 1; j <= i; j++ ){
-           System.out.print("* ");
-       }
-       System.out.println();
+//    for(int i = 1; i <= n; i++){
+//       for(int j = 1; j <= i; j++ ){
+//           System.out.print("* ");
+//       }
+//       System.out.println();
+//    }
+
+    for(int row = 1; row<=n; row++){
+        for(int col = 1; col<=6; col++){
+            if(row == 1 || row == n){
+                System.out.print("* ");
+            }
+            else{
+                if(col == 1 || col == 6){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+        }
+        //move to next row
+        System.out.println();
     }
 
 
