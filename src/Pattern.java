de6baa881040ca4276fef2aa9c8b5bb9d3cea4dc
Ravinder Tiwari <ravindertiwari373@gmail.com>
2026-05-31 23:57:33 +0530

    Comment out pattern generation code in Pattern.java

--- src/Pattern.java
+++ src/Pattern.java
@@ -36,56 +36,67 @@ void main() {
     //Pattern 3
 
     //Part1
-    for(int row = 1; row<=n; row++){
+    // for(int row = 1; row<=n; row++){
 
-        for(int col = 1; col<=n-row; col++){
-            System.out.print("  ");
-        }
+    //     for(int col = 1; col<=n-row; col++){
+    //         System.out.print("  ");
+    //     }
 
-        if(row==1){
-            for(int col=1; col<=2*row-1;col++){
-                System.out.print("* ");
-            }
-        }
-        else{
-            System.out.print("* ");
+    //     if(row==1){
+    //         for(int col=1; col<=2*row-1;col++){
+    //             System.out.print("* ");
+    //         }
+    //     }
+    //     else{
+    //         System.out.print("* ");
 
-            for(int col=1; col<=2*row-3; col++){
-                System.out.print("  ");
-            }
+    //         for(int col=1; col<=2*row-3; col++){
+    //             System.out.print("  ");
+    //         }
 
-            System.out.print("* ");
+    //         System.out.print("* ");
 
-        }
-        //move to next row
-        System.out.println();
-    }
+    //     }
+    //     //move to next row
+    //     System.out.println();
+    // }
 
     //part2 (below)
-    for(int row = 1; row<=(n-1); row++){
-
         //part1
-        for(int col = 1; col<=row; col++){
-            System.out.print("  ");
-        }
+    //     for(int col = 1; col<=row; col++){
+    //         System.out.print("  ");
+    //     }
 
-        if(row == (n-1)){
-            System.out.print("* ");
-        }
-        else{
-            System.out.print("* ");
+    //     if(row == (n-1)){
+    //         System.out.print("* ");
+    //     }
+    //     else{
+    //         System.out.print("* ");
 
-            for(int col=1; col<=2*(n-row)-3; col++){
-                System.out.print("  ");
-            }
+    //         for(int col=1; col<=2*(n-row)-3; col++){
+    //             System.out.print("  ");
+    //         }
 
-            System.out.print("* ");
+    //         System.out.print("* ");
 
-        }
-        System.out.println();
-    }
+    //     }
+    //     System.out.println();
+    // }
 
     //Pattern4"
+            int n = 5;
+            for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n-i; j++)
+                System.out.print(" ");
+
+            for(int k = 1; k <= 2*i-1; k++) {
+                if(k == 1 || k == 2*i-1 || i == n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
 
 
     //Pattern5
